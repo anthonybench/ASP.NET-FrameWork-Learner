3b3953689e5e110cb2178fa660f6e8dd30c3b3e7
Sleepy Boy <36245150+anthonybench@users.noreply.github.com>
2020-06-12 11:39:44 -0700

    Added series repository

---
+++ src/ComicBookGallery/ComicBookGallery/Data/SeriesRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Data
+{
+    public class SeriesRepository
+    {
+        public Series[] GetSeries()
+        {
+            return Data.Series;
+        }
+
+        public Series GetSeriesDetail(int id)
+        {
+            Series seriesToReturn = null;
+
+            foreach (var series in Data.Series)
+            {
+                if (id == series.Id)
+                {
+                    seriesToReturn = series;
+                    break;
+                }
+            }
+            if (seriesToReturn != null)
+            {
+                var comicBooks = new ComicBook[0];
+                foreach (var comicBook in Data.ComicBooks)
+                {
+                    if (comicBook.Series != null && comicBook.Series.Id == id)
+                    {
+                        Array.Resize(ref comicBooks, comicBooks.Length + 1);
+                        comicBooks[comicBooks.Length - 1] = comicBook;
+                    }
+                }
+
+                seriesToReturn.Issues = comicBooks;
+            }
+
+            return seriesToReturn;
+        }
+    }
+}
